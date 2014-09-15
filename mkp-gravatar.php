ed3e31299790e371c71af5f9a9d1a004c979451a
Michael K Pate <michaelkpate@gmail.com>
2014-09-15 15:16:20 -0400

    Update mkp-gravatar.php

--- mkp-gravatar.php
+++ mkp-gravatar.php
@@ -6,25 +6,33 @@
 		extract(lAtts(array(
 			'email'   => '',	
 			'size'   => '80',
-			'default'   => 'mm',
+			'default'   => '',
 			'rating'   => 'g',
 			'secure'   => '0',	
-			'image'   => '1',				
-			'class'  => ''
+			'image'   => '1',	
+			'class'   => '',		
+			'alt'  => ''
 		),$atts));
 		
-		$http = (isset($secure)) ? 'https://' : 'http://';
+		$http = ($secure=='1') ? 'https://' : 'http://';
 		
 		$email 	= (!empty($email)) ? md5(strtolower(trim(parse($email)))) : md5(strtolower(trim($thiscomment['email'])));
  
-		$url = $http.'www.gravatar.com/avatar/'.$email.'?s='.$size.'&d='.$default.'&r='.$rating ;
+		$url = $http.'www.gravatar.com/avatar/';
+		$url .= $email;
+		$url .= 's='.$size;
+		$url .= '&d='.$default;
+		$url .= '&r='.$rating;
 		
 		if (isset($image)) {
-			if (isset($class)) {		
-				$url = '<img src="' . $url . '" />';
-			} else {
-				$url = '<img class="'.$class.'". src="' . $url . '" />';
+			$url = '<img src="' . $url . '" ';
+			if (!empty($class)) {		
+				$url .= 'class="' . $class . '" ';
 			}
+			if (!empty($alt)) {		
+				$url .= 'alt="' . $alt . '" ';
+			}
+			$url .= '/>';
 		}
  
 		return $url;
