7bb84e3ae58794ea75eccf265a4c5b20f440694f
Michael K Pate <michaelkpate@gmail.com>
2014-09-16 17:08:07 -0400

    Update mkp_gravatar.php

--- mkp_gravatar.php
+++ mkp_gravatar.php
@@ -4,40 +4,48 @@
 		global $thiscomment;
 	
 		extract(lAtts(array(
+			'secure'   => '0',
 			'email'   => '',	
 			'size'   => '80',
 			'rating'   => 'g',
 			'default'   => '',
-			'force'   => '',
-			'secure'   => '0',	
-			'image'   => '1',	
-			'class'   => '',		
+			'force'   => '',	
+			'image'   => '1',
 			'alt'  => '',
+			'class'   => '',		
+			'id'  => '',
+			'style'  => '',
+			'title'  => '',
 			'disable'  => '',
 		),$atts));
 		
-		$http = ($secure=='1' || $secure=='y' || $secure=='true') ? 'https://' : 'http://';
+		$url = ($secure=='1' || $secure=='y' || $secure=='true') ? 'https://secure.gravatar.com/avatar/' : 'http://www.gravatar.com/avatar/';
 		
-		$email 	= (!empty($email)) ? md5(strtolower(trim(parse($email)))) : md5(strtolower(trim($thiscomment['email'])));
- 
-		$url = $http.'www.gravatar.com/avatar/';
-		$url .= $email;
-		$url .= '?s='.$size;
-		$url .= '&r='.$rating;
+		$url .= (!empty($email)) ? md5(strtolower(trim(parse($email)))) : md5(strtolower(trim($thiscomment['email'])));
+
+		$url .= '?s='. $size . '&r=' . $rating;
 
 		if (!empty($default)) {
 			$url .= '&d='.$default;
-			$force = ($force=='1' || $force=='y' || $force=='true') ? '&f=y' : '';
-			$url .= $force;
+			$url .= ($force=='1' || $force=='y' || $force=='true') ? '&f=y' : '';
 		}
 		
-		if ($image=='1' || $image=='y' || $secure=='true') {
-			$url = '<img src="' . $url . '" ';
+		if ($image=='1' || $image=='y' || $image=='true') {
+			$url = '<img src="' . $url . '" height="' . $size . '" width="' . $size . '" ';
+			if (!empty($alt)) {		
+				$url .= 'alt="' . $alt . '" ';
+			}
 			if (!empty($class)) {		
 				$url .= 'class="' . $class . '" ';
 			}
-			if (!empty($alt)) {		
-				$url .= 'alt="' . $alt . '" ';
+			if (!empty($id)) {		
+				$url .= 'id="' . $id . '" ';
+			}
+			if (!empty($style)) {		
+				$url .= 'style="' . $style . '" ';
+			}
+			if (!empty($title)) {		
+				$url .= 'title="' . $title . '" ';
 			}
 			$url .= '/>';
 		}
